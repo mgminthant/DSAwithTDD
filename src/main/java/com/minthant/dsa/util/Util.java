79cddc1593da076877587383e3c6f68719082ca2
mgminthant <mterror2004@gmail.com>
2025-11-01 12:43:56 +0630

    add bubble sort with rich test case

--- src/main/java/com/minthant/dsa/util/Util.java
+++ src/main/java/com/minthant/dsa/util/Util.java
@@ -1,12 +1,36 @@
 package com.minthant.dsa.util;
 
+import java.util.Random;
+
 public class Util {
+
     public boolean isArray(int [] arr){
+
         for(int i = 0; i < arr.length - 1; i++ ){
+
             if(arr[i] > arr[i+1]){
                 return false;
             }
+
         }
+
         return true;
+
     }
+
+    public int[] createRandomArray (int arraySize){
+
+        int [] arr = new int[arraySize];
+
+        Random random = new Random();
+
+        for(int i = 0; i < arr.length; i++){
+
+            arr[i] = random.nextInt(400);
+
+        }
+
+        return arr;
+    }
+
 }
---
+++ src/test/java/com/minthant/dsa/sorting/SortingTest.java
@@ -0,0 +1,61 @@
+package com.minthant.dsa.sorting;
+
+import com.minthant.dsa.util.Util;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SortingTest {
+
+    Util util = new Util();
+    SortingAlgorithm sortingAlgo = new BubbleSort();
+
+    @Test
+    public void testSort(){
+
+        int [] arr = {5,2,10,0,25,1};
+        sortingAlgo.sort(arr);
+
+        Assertions.assertTrue(util.isArray(arr));
+
+    }
+
+    @Test
+    public void testWithEmptyArray(){
+
+        int [] arr = {};
+        sortingAlgo.sort(arr);
+
+        Assertions.assertTrue(util.isArray(arr));
+
+    }
+
+    @Test
+    public void testWithSingleArrayElement(){
+
+        int [] arr = {1};
+        sortingAlgo.sort(arr);
+
+        Assertions.assertTrue(util.isArray(arr));
+
+    }
+
+    @Test
+    public void testWithDoubleElement(){
+
+        int [] arr = {7,2};
+        sortingAlgo.sort(arr);
+
+        Assertions.assertTrue(util.isArray(arr));
+    }
+
+    @Test
+    public void testWithRandomData(){
+
+        int [] arr = util.createRandomArray(50);
+        sortingAlgo.sort(arr);
+
+        Assertions.assertTrue(util.isArray(arr));
+
+    }
+
+}
